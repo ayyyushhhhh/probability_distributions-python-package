fbf82d577ff811801071f04a08b05d05a82437ff
ayyyushhhhh <ayush002rawat@gmail.com>
2021-09-20 13:05:25 +0530

    gaussian distribution

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup(name='probability_distributions',
+      version='1.0',
+      description='This pacakage contains probability distributions like Gaussian Distribution etc.',
+      author='Ayush Rawat',
+      author_email='ayush.rawat19@icloud.com',
+      packages=['distributions'],
+      zip_safe = False
+     )
